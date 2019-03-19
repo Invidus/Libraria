8edd9ef6c7faccceb13c0e66021f8bdf355143c0
Invidus <45075021+Invidus@users.noreply.github.com>
2019-03-19 15:43:36 +0300

    3

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Page Title</title> 
 
     <link rel="stylesheet" type="text/css" href="Header.css" />
 </head>
